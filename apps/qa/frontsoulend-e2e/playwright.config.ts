5db0a16d738471d11dde9110e4be5017ae4ee5dd
Kostiantyn Kifor <ks.kifor@gmail.com>
2024-11-08 01:15:08 +0100

    ci [DEV-1]: Update workflow

--- apps/qa/frontsoulend-e2e/playwright.config.ts
+++ apps/qa/frontsoulend-e2e/playwright.config.ts
@@ -7,7 +7,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
 
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv.
+ * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
 
