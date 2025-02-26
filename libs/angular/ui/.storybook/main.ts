a21f563d7123df3464326595b35bf9a68c1cff87
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-02-26 19:32:16 +0100

    feat [DEV-27]: Add new storybook addons

--- libs/angular/ui/.storybook/main.ts
+++ libs/angular/ui/.storybook/main.ts
@@ -2,7 +2,14 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
 	stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-	addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+	addons: [
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		'@storybook/addon-links',
+		'@storybook/addon-queryparams',
+		'@storybook/addon-storysource',
+	],
 	framework: {
 		name: '@storybook/angular',
 		options: {},
