0324fed4666971a92b8c5deb230a4586d13f7fa5
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-02-26 21:19:30 +0100

    feat [DEV-27]: Add storybook preview default options

--- libs/angular/ui/.storybook/preview.ts
+++ libs/angular/ui/.storybook/preview.ts
@@ -1,16 +1,24 @@
-import type { Preview } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+
+import { moduleMetadata, Preview } from '@storybook/angular';
 
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: '^on[A-Z].*' },
 		layout: 'centered',
 		controls: {
+			sort: 'alpha',
 			matchers: {
 				color: /(?<temp1>background|color)$/iu,
 				date: /Date$/u,
 			},
 		},
 	},
+	decorators: [
+		moduleMetadata({
+			imports: [CommonModule],
+		}),
+	],
 };
 
 export default preview;
