a4f6f3de2b63fb372bcd87c8c27fc7d0ae59d7fe
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-02-27 21:51:23 +0100

    feat [DEV-27]: Provide experimental zoneless change detection

--- apps/frontend/angular/platform/src/app.config.ts
+++ apps/frontend/angular/platform/src/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+	ApplicationConfig,
+	provideExperimentalZonelessChangeDetection,
+	provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(appRoutes),
+	],
 };
