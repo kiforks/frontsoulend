8e45d6c6969c96cecfb24f2955308a575ad3d1a2
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-04-14 23:31:01 +0200

    refactor [DEV-49]: Restructure project to improve component organization

--- apps/frontend/angular/platform/src/app/app.config.ts
+++ apps/frontend/angular/platform/src/app/app.config.ts
@@ -1,16 +1,8 @@
-import {
-	ApplicationConfig,
-	provideExperimentalZonelessChangeDetection,
-	provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideExperimentalZonelessChangeDetection(),
-		provideRouter(appRoutes),
-	],
+	providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes)],
 };
--- apps/frontend/angular/platform/src/app/main.ts
+++ apps/frontend/angular/platform/src/app/main.ts
@@ -2,6 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app.config';
 
-import { RootComponent } from './components';
+import { RootComponent } from '../pages';
 
 bootstrapApplication(RootComponent, appConfig).catch(err => console.error(err));
