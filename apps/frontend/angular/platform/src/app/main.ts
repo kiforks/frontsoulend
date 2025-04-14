2dd48251a011186a51909a02ee5feaa5decb66a1
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-04-14 22:06:45 +0200

    refactor [DEV-49]: Reorganize project files into app directory

--- apps/frontend/angular/platform/src/app/main.ts
+++ apps/frontend/angular/platform/src/app/main.ts
@@ -2,6 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app.config';
 
-import { RootComponent } from './app/components';
+import { RootComponent } from './components';
 
 bootstrapApplication(RootComponent, appConfig).catch(err => console.error(err));
