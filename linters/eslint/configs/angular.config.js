99acd4a21e5a957311bb1e5a1d6e148be89d55d3
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-04-23 20:21:35 +0200

    feat [DEV-49]: Update Angular ESLint config with i18n rules for HTML files

--- linters/eslint/configs/angular.config.js
+++ linters/eslint/configs/angular.config.js
@@ -4,4 +4,65 @@ import kiforAngularTypescript from '@kiforks/eslint-config/angular-typescript.js
 import baseConfig from '../../../eslint.config.js';
 
 /** @type { import("eslint").Linter.Config[] } */
-export default [...baseConfig, ...kiforAngularTemplate, ...kiforAngularTypescript];
+export default [
+	...baseConfig,
+	...kiforAngularTemplate,
+	...kiforAngularTypescript,
+
+	/** TODO update global eslint config */
+	{
+		files: ['**/*.html'],
+		rules: {
+			'@angular-eslint/template/i18n': [
+				'error',
+				{
+					allowMarkupInContent: true,
+					checkAttributes: true,
+					checkDuplicateId: true,
+					checkId: false,
+					checkText: true,
+					ignoreAttributes: [
+						'href',
+						'content',
+						'rel',
+						'src',
+						'id',
+						'class',
+						'style',
+						'target',
+						'type',
+						'value',
+						'width',
+						'height',
+						'xmlns',
+						'tabindex',
+						'role',
+						'routerLink',
+						'routerLinkActive',
+						'name',
+						'list',
+						'fill',
+						'stroke',
+						'stroke-width',
+						'viewBox',
+						'svgIcon',
+						'autocomplete',
+						'charset',
+						'colspan',
+						'dir',
+						'for',
+						'lang',
+						'formArrayName',
+						'formControlName',
+						'formGroupName',
+						'ngClass',
+						'ngProjectAs',
+					],
+					ignoreTags: ['style', 'script', 'meta', 'link'],
+					requireDescription: false,
+					requireMeaning: false,
+				},
+			],
+		},
+	},
+];
