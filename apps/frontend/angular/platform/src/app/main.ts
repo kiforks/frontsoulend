67d9c97284cd7d5d43bc1c2ae53b426b364abece
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-04-23 20:21:18 +0200

    feat [DEV-49]: Add i18n support with @angular/localize library

--- apps/frontend/angular/platform/src/app/main.ts
+++ apps/frontend/angular/platform/src/app/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app.config';
 
+import './polyfills';
+
 import { RootComponent } from '../pages';
 
 bootstrapApplication(RootComponent, appConfig).catch(err => console.error(err));
