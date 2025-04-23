27056448f19e8800117729bdc71500b1b7acab4c
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-04-23 21:59:40 +0200

    refactor [DEV-49]: Remove polyfills import from main.ts and update project.json configuration

--- apps/frontend/angular/platform/src/app/main.ts
+++ apps/frontend/angular/platform/src/app/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app.config';
 
-import './polyfills';
-
 import { RootComponent } from '../pages';
 
 bootstrapApplication(RootComponent, appConfig).catch(err => console.error(err));
