c6ec61556d0f3d94d31a7bcddec8d3404a5f6975
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-06-02 21:51:13 +0200

    refactor [DEV-49]: Update ESLint config for NestJS with scoped rules

--- linters/eslint/configs/nestjs.config.js
+++ linters/eslint/configs/nestjs.config.js
@@ -4,23 +4,31 @@ import nestJsConfig from 'eslint-plugin-nestjs';
 
 /* TODO Move to a separate config package */
 export default [
-	{
-		plugins: {
-			nestjs: nestJsConfig,
+	...[
+		{
+			plugins: {
+				nestjs: nestJsConfig,
+			},
+			rules: {
+				'nestjs/parse-int-pipe': 'error',
+				'nestjs/deprecated-api-modules': 'error',
+				'nestjs/use-dependency-injection': 'error',
+				'nestjs/use-validation-pipe': 'error',
+			},
 		},
+		...eslintNestJsTyped.configs.flatRecommended,
+		{
+			rules: {
+				/* This rule is not working properly */
+				'@darraghor/nestjs-typed/validated-non-primitive-property-needs-type-decorator': 'off',
+				'nestjs/use-validation-pipe': 'off',
+			},
+		},
+	].map(config => ({ ...config, files: ['**/*.ts'] })),
+	{
+		files: ['**/*.controller.ts'],
 		rules: {
-			'nestjs/parse-int-pipe': 'error',
-			'nestjs/deprecated-api-modules': 'error',
-			'nestjs/use-dependency-injection': 'error',
-			'nestjs/use-validation-pipe': 'error',
+			'class-methods-use-this': 'off',
 		},
 	},
-	...eslintNestJsTyped.configs.flatRecommended,
-	{
-		rules: {
-			/* This rule is not working properly */
-			'@darraghor/nestjs-typed/validated-non-primitive-property-needs-type-decorator': 'off',
-			'nestjs/use-validation-pipe': 'off',
-		},
-	},
-].map(config => ({ ...config, files: ['**/*.ts'] }));
+];
