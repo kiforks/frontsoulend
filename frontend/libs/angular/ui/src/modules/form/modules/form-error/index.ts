c114d7a51583479c14ecd4cc370181f89e108fd9
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-08-13 20:06:59 +0200

    feat [DEV-49]: Add public index for form error module to centralize exports and improve reusability

---
+++ frontend/libs/angular/ui/src/modules/form/modules/form-error/index.ts
@@ -0,0 +1,24 @@
+export { FormErrorDirective } from './directives';
+export {
+	FormErrorMessages,
+	FormErrorComponent,
+	FormErrorComponentData,
+	FormErrorComponentConfig,
+	FormErrorMessageConfig,
+	FormErrorMessageHandler,
+	FormErrorMessageFilter,
+	FormErrorMessageData,
+	FormErrorConfig,
+	FormErrorValidationType,
+} from './interfaces';
+export { FORM_ERROR_MESSAGES, FORM_ERROR_CONFIG } from './tokens';
+export { FormErrorHelper, FormErrorValidationHelper, FormErrorMessageHelper } from './helpers';
+export {
+	FormErrorConfigMock,
+	FormErrorMessagesMock,
+	FormErrorMessageConfigMock,
+	FormErrorComponentConfigMock,
+	FormErrorMessagesMockParams,
+	FormErrorMessagesLengthParams,
+} from './mocks';
+export { FormErrorOptionsConfig } from './configs';
