c3f9cc22e3e021002b9757a472d3b2b444ccf231
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-09-08 18:45:55 +0200

    build [DEV-49]: Upgrade Angular and Nx versions in frontend to support latest features and fixes

--- frontend/apps/angular/platform/src/app/app.config.ts
+++ frontend/apps/angular/platform/src/app/app.config.ts
@@ -4,6 +4,5 @@ import { provideRouter } from '@angular/router';
 import { Routes } from '../pages';
 
 export const appConfig: ApplicationConfig = {
-	// eslint-disable-next-line @angular-eslint/no-developer-preview
 	providers: [provideZonelessChangeDetection(), provideRouter(Routes)],
 };
