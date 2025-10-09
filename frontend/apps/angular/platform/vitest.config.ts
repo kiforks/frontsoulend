1d8a351554cabef1c998f4db4b488ef2551bb629
Kostiantyn Kifor <ks.kifor@gmail.com>
2025-10-09 20:01:24 +0200

    refactor [DEV-49]: Reorder test config options in vitest.config.ts for clarity and maintainability

--- frontend/apps/angular/platform/vitest.config.ts
+++ frontend/apps/angular/platform/vitest.config.ts
@@ -10,21 +10,22 @@ export default defineConfig(() => ({
 	cacheDir: '../../../../node_modules/.vite/frontend/apps/angular/platform',
 	plugins: [angular(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
 	test: {
-		name: 'frontend/apps/angular/platform',
-		watch: false,
-		globals: true,
-		environment: 'jsdom',
-		include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		setupFiles: ['vitest.environment.ts'],
-		reporters: ['default'],
 		coverage: {
 			reportsDirectory: '../../../../coverage/frontend/apps/angular/platform',
 			provider: 'v8' as const,
 		},
+		environment: 'jsdom',
+		globals: true,
+		include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		isolate: true,
+		name: 'frontend/apps/angular/platform',
+		reporters: ['default'],
 		server: {
 			deps: {
 				inline: ['@ngneat/spectator'],
 			},
 		},
+		setupFiles: ['vitest.environment.ts'],
+		watch: false,
 	},
 }));
